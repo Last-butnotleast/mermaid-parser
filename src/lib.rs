49c5ca1ca114eeed8c53d3c013509adccecdd834
tom <tom@elbtech.dev>
2025-06-06 22:31:05 +0200

    basic lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
-mod ast;
+use pest::Parser;
+use pest::iterators::Pairs;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar/mermaid.pest"]
+pub struct MermaidParser;
+
+pub fn parse_diagram(input: &str) -> Result<Pairs<'_, Rule>, pest::error::Error<Rule>> {
+    MermaidParser::parse(Rule::file, input)
+}
