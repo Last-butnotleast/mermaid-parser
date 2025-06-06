dba220253756671becc9e39a3fb963f39d0ba5dc
tom <tom@elbtech.dev>
2025-06-06 23:13:49 +0200

    add lib return type

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,16 @@
 mod types;
 
 use pest::Parser;
-use pest::iterators::Pairs;
 use pest_derive::Parser;
+use crate::types::Diagram;
 
 #[derive(Parser)]
 #[grammar = "grammar/mermaid.pest"]
 pub struct MermaidParser;
 
-pub fn parse_diagram(input: &str) -> Result<Pairs<'_, Rule>, pest::error::Error<Rule>> {
-    MermaidParser::parse(Rule::file, input)
+pub fn parse_diagram(input: &str) -> Result<Diagram, pest::error::Error<Rule>> {
+    let res =  MermaidParser::parse(Rule::file, input)?; 
+    println!("{:#?}", res);
+    
+    Ok(Diagram::default())
 }
